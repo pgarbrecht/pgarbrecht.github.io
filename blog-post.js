2320d8f112c1bbe0b5fcd5a0e8cf6e69e6f58da1
Phil Garbrecht <88944137+pgarbrecht@users.noreply.github.com>
2022-03-20 11:06:02 -0500

    Delete blog-post.js

--- blog-post.js
+++
@@ -1,9 +0,0 @@
-var button = document.createElement("button");
-button.innerHTML = "Connect with Phil on LinkedIn";
-
-var body = document.getElementsByTagName("body")[0];
-body.appendChild(button);
-
-button.addEventListener ("click", function() {  
-  onclick="https://www.linkedin.com/in/philgarbrecht/";
-});
