ab747f68d644b2a9a49218c1cccb94b5e7c31e6b
Phil Garbrecht <88944137+pgarbrecht@users.noreply.github.com>
2022-05-17 07:39:17 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+//show and hide dropdown list items on button click  
+function show_hide() {  
+   var click = document.getElementById("list-items");  
+   if(click.style.display ==="none") {  
+      click.style.display ="block";  
+   } else {  
+      click.style.display ="none";  
+   }   
+}  
