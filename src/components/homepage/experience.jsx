14fd3dca08fc06adb2f1eda8bb286fde279464a2
pgarbrecht <philgarbrecht@gmail.com>
2024-09-17 11:26:08 -0500

    content update
    Signed-off-by: pgarbrecht <philgarbrecht@gmail.com>

--- src/components/homepage/experience.jsx
+++ src/components/homepage/experience.jsx
@@ -22,7 +22,7 @@ const Experience = ({ refProps }) => {
 							/>
 							<div className="work-text-fields">
 								<div className="work-title">Sisu</div>
-								<div className="work-subtitle">Software Developer</div>
+								<div className="work-subtitle">Web Software Developer</div>
 								<div className="work-duration">January 2023 - Present</div>
 							</div>
 						</div>
@@ -35,7 +35,7 @@ const Experience = ({ refProps }) => {
 							/>
 							<div className="work-text-fields">
 								<div className="work-title">Watermark</div>
-								<div className="work-subtitle">Senior Web Developer</div>
+								<div className="work-subtitle">Senior Website Manager</div>
 								<div className="work-duration">
 									September 2021 - January 2023
 								</div>
@@ -45,7 +45,7 @@ const Experience = ({ refProps }) => {
 							<div className="work-image-placeholder" />
 							<div className="work-text-fields">
 								<div className="work-title"></div>
-								<div className="work-subtitle">Web Developer</div>
+								<div className="work-subtitle">Website Manager</div>
 								<div className="work-duration">
 									September 2020 - September 2021
 								</div>
@@ -60,7 +60,7 @@ const Experience = ({ refProps }) => {
 							/>
 							<div className="work-text-fields">
 								<div className="work-title">Khoros</div>
-								<div className="work-subtitle">Web Project Developer</div>
+								<div className="work-subtitle">Website Program Manager</div>
 								<div className="work-duration">
 									January 2020 - September 2020
 								</div>
@@ -70,7 +70,7 @@ const Experience = ({ refProps }) => {
 							<div className="work-image-placeholder" />
 							<div className="work-text-fields">
 								<div className="work-title"></div>
-								<div className="work-subtitle">Web Content Developer</div>
+								<div className="work-subtitle">Website Content Manager</div>
 								<div className="work-duration">January 2019 - January 2020</div>
 							</div>
 						</div>
