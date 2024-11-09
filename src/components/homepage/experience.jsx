0b2fba16b6a61825231ac8958d2f584163b9735c
pgarbrecht <philgarbrecht@gmail.com>
2024-11-09 08:30:06 -0600

    updates content
    Signed-off-by: pgarbrecht <philgarbrecht@gmail.com>

--- src/components/homepage/experience.jsx
+++ src/components/homepage/experience.jsx
@@ -35,7 +35,9 @@ const Experience = ({ refProps }) => {
 							/>
 							<div className="work-text-fields">
 								<div className="work-title">Watermark</div>
-								<div className="work-subtitle">Senior Web Developer</div>
+								<div className="work-subtitle">
+									Senior Website Manager and Developer
+								</div>
 								<div className="work-duration">
 									September 2021 - January 2023
 								</div>
@@ -45,7 +47,9 @@ const Experience = ({ refProps }) => {
 							<div className="work-image-placeholder" />
 							<div className="work-text-fields">
 								<div className="work-title"></div>
-								<div className="work-subtitle">Web Developer</div>
+								<div className="work-subtitle">
+									Website Manager and Developer
+								</div>
 								<div className="work-duration">
 									September 2020 - September 2021
 								</div>
@@ -60,7 +64,9 @@ const Experience = ({ refProps }) => {
 							/>
 							<div className="work-text-fields">
 								<div className="work-title">Khoros</div>
-								<div className="work-subtitle">Web Project Developer</div>
+								<div className="work-subtitle">
+									Website Project Manager and Developer
+								</div>
 								<div className="work-duration">
 									January 2020 - September 2020
 								</div>
@@ -70,7 +76,9 @@ const Experience = ({ refProps }) => {
 							<div className="work-image-placeholder" />
 							<div className="work-text-fields">
 								<div className="work-title"></div>
-								<div className="work-subtitle">Web Content Developer</div>
+								<div className="work-subtitle">
+									Website Content Manager and Developer
+								</div>
 								<div className="work-duration">January 2019 - January 2020</div>
 							</div>
 						</div>
